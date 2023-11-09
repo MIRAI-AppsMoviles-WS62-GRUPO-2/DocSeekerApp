a7b83157737383761f28c910020e62985cff47bf
William Riega <williammar954@hotmail.com>
2023-11-08 19:21:54 -0500

    Add feature doctors filters sheet

--- lib/professionalList.dart
+++ lib/professionalList.dart
@@ -3,6 +3,10 @@ import 'package:doc_seeker_app/sidebar.dart';
 import 'package:flutter/material.dart';
 import 'package:doc_seeker_app/doctors/api/Service.dart';
 
+enum specialities {Todos, Dermatolgia, Podologia, General, Psiquiatria}
+enum estrellas {Todos, Una, Dos, Tres, Cuatro, Cinco}
+enum precios {Todos, Menor, Mayor}
+
 class ListProfessional extends StatefulWidget {
     const ListProfessional({super.key});
 
@@ -11,12 +15,15 @@ class ListProfessional extends StatefulWidget {
   }
 
 class _ListProfessionalState extends State<ListProfessional> {
-    @override
+  specialities? _speciality = specialities.Todos;
+  estrellas? _stars = estrellas.Todos;
+  precios? _cost = precios.Todos;
+  @override
     Widget build(BuildContext context) {
       return Scaffold(
         endDrawer: const MyDrawer(),
         appBar: AppBar(
-          title: const Text("Elegir al profesioanl", style: TextStyle(color: Colors.white),),
+          title: const Text("Elegir al profesional", style: TextStyle(color: Colors.white),),
           backgroundColor: const Color(0xff0093AB),
           centerTitle: true,
           actions: <Widget>[
@@ -60,9 +67,241 @@ class _ListProfessionalState extends State<ListProfessional> {
                 style: TextButton.styleFrom(
                     backgroundColor: Color(0xffFD5D5D)
                 ),
-                  onPressed: (){},
+                  onPressed: (){
+                    showModalBottomSheet(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                      ),
+                        context: context,
+                        builder: (BuildContext context){
+                          return StatefulBuilder(
+                            builder: (BuildContextcontext, StateSetter setState) {
+                              return Container(
+                                padding: EdgeInsets.all(16),
+                                  child: ListView(
+                                      children:[
+                                        Row(
+                                          children: [
+                                            const Expanded(
+                                                child: Center(
+                                                    child:
+                                                    Text("Filtros", style: TextStyle(
+                                                        color: Color(0xff0093AB),
+                                                        fontWeight: FontWeight.bold,
+                                                        fontSize: 20)
+                                                    )
+                                                )
+                                            ),
+                                            IconButton(
+                                                onPressed: (){Navigator.pop(context);},
+                                                icon: Icon(Icons.close, size: 16,))
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                                          child: Text("Especialidad", style: TextStyle(color: Colors.grey, fontSize: 16, fontWeight: FontWeight.bold)),
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Todos", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: specialities.Todos,
+                                                  groupValue: _speciality,
+                                                  onChanged: (specialities? value){
+                                                    setState(() {
+                                                      _speciality = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Medicina General", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: specialities.General,
+                                                  groupValue: _speciality,
+                                                  onChanged: (specialities? value){
+                                                    setState(() {
+                                                      _speciality = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Dematología", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: specialities.Dermatolgia,
+                                                  groupValue: _speciality,
+                                                  onChanged: (specialities? value){
+                                                    setState(() {
+                                                      _speciality = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Podología", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: specialities.Podologia,
+                                                  groupValue: _speciality,
+                                                  onChanged: (specialities? value){
+                                                    setState(() {
+                                                      _speciality = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Psquiatría", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: specialities.Psiquiatria,
+                                                  groupValue: _speciality,
+                                                  onChanged: (specialities? value){
+                                                    setState(() {
+                                                      _speciality = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 24.0, bottom: 8.0),
+                                          child: Text("Estrellas", style: TextStyle(color: Colors.grey, fontSize: 16, fontWeight: FontWeight.bold)),
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Todos", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: estrellas.Todos,
+                                                  groupValue: _stars,
+                                                  onChanged: (estrellas? value){
+                                                    setState(() {
+                                                      _stars = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("1 Estrellas", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: estrellas.Una,
+                                                  groupValue: _stars,
+                                                  onChanged: (estrellas? value){
+                                                    setState(() {
+                                                      _stars = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("2 Estrellas", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: estrellas.Dos,
+                                                  groupValue: _stars,
+                                                  onChanged: (estrellas? value){
+                                                    setState(() {
+                                                      _stars = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("3 Estrellas", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: estrellas.Tres,
+                                                  groupValue: _stars,
+                                                  onChanged: (estrellas? value){
+                                                    setState(() {
+                                                      _stars = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("4 Estrellas", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: estrellas.Cuatro,
+                                                  groupValue: _stars,
+                                                  onChanged: (estrellas? value){
+                                                    setState(() {
+                                                      _stars = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("5 Estrellas", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: estrellas.Cinco,
+                                                  groupValue: _stars,
+                                                  onChanged: (estrellas? value){
+                                                    setState(() {
+                                                      _stars = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 24.0, bottom: 8.0),
+                                          child: Text("Precio", style: TextStyle(color: Colors.grey, fontSize: 16, fontWeight: FontWeight.bold)),
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Todos", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: precios.Todos,
+                                                  groupValue: _cost,
+                                                  onChanged: (precios? value){
+                                                    setState(() {
+                                                      _cost = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Menos de S/. 50", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: precios.Menor,
+                                                  groupValue: _cost,
+                                                  onChanged: (precios? value){
+                                                    setState(() {
+                                                      _cost = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                        Row(
+                                            children: [
+                                              Expanded(child: Text("Más de S/. 50", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700))),
+                                              Radio(
+                                                  value: precios.Mayor,
+                                                  groupValue: _cost,
+                                                  onChanged: (precios? value){
+                                                    setState(() {
+                                                      _cost = value;
+                                                    });
+                                                  })
+                                            ]
+                                        ),
+                                      ]
+                                  )
+                              );
+                            }
+                          );
+                        }
+                    );
+                  },
                   child: Text("Filtrar")),
             ),
+
             FutureBuilder(
               initialData: [],
               future: DoctorService.getDoctors(),
