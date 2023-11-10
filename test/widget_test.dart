17243b9cf5127d71eb9e4bd91efe0e9cd0c08dea
Gabriela Nomberto Ramos <102630015+GabySol26@users.noreply.github.com>
2023-11-10 18:11:21 -0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:doc_seeker_app/main.dart';
+import 'package:dockseekerapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
