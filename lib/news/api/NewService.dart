dacb3c8d95da5d37a1ca074e970abbea79f931e5
Dennis Piero Quevedito <u201619823@upc.edu.pe>
2023-11-23 09:44:12 -0500

    News implementado

--- lib/news/api/NewService.dart
+++ lib/news/api/NewService.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'New.dart';
 import 'ListNew.dart';
 
-class DoctorService {
+class NewService {
   static Future<List<New>> getNews() async {
     final rspta = await http.get(
-        //Uri.parse('https://docseekerapi.azurewebsites.net/api/v1/doctors'));
-        Uri.parse('https://localhost:7225/api/v1/new'));
+      Uri.parse('https://docseekerapi.azurewebsites.net/api/v1/new'));
+      //Uri.parse('http://10.0.2.2:7225/api/v1/new'));
     if (rspta.statusCode == 200) {
       final rsptaJson = json.decode(rspta.body);
       final listaNews = ListNews.listado(rsptaJson);
